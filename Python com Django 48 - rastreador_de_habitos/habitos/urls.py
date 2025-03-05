da4f8a410044184e5ae162a750b2e95926591fd9
Luan Ramalho <32373552+LuanRamalho@users.noreply.github.com>
2025-03-05 12:09:53 -0300

    urls.py

--- Python com Django 48 - rastreador_de_habitos/habitos/urls.py
+++ Python com Django 48 - rastreador_de_habitos/habitos/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('listar/', views.listar_habitos, name='listar_habitos'),
     path('registrar/<int:habito_id>/', views.registrar_habito, name='registrar_habito'),
     path('relatorio/<int:habito_id>/', views.relatorio_habito, name='relatorio_habito'),
+    path('editar/<int:habito_id>/', views.editar_habito, name='editar_habito'),
+    path('excluir/<int:habito_id>/', views.excluir_habito, name='excluir_habito'),
+    path("habitos/editar_registro/<int:registro_id>/", views.editar_registro, name="editar_registro"),
+    path("habitos/excluir_registro/<int:registro_id>/", views.excluir_registro, name="excluir_registro"),
 ]
